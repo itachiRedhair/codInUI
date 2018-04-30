fbd13f6247d18cf6ef58bdfd72b41d69100d4c89
Souvik Chakraborty <souvik.c5@tcs.com>
2018-04-30 10:20:48 +0530

    added signup component

--- src/pages/Homepage/HomepageComponent/HomepageComponent.jsx
+++ src/pages/Homepage/HomepageComponent/HomepageComponent.jsx
@@ -7,15 +7,37 @@ import Signup from "./../SignupComponent";
 import "./HomepageComponent.scss";
 
 export default class HomepageComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLogin: true
+        }
+    }
+    toggleLogin(){
+        this.setState({
+            isLogin: true
+        })
+    }
+    toggleSignup() {
+        this.setState({
+            isLogin: false
+        })
+    }
   render() {
     return (
       <div className="homepage-container">
         <Grid>
           <Row>
             <Col md={6} mdPush={6} className="login-form-container">
+              <div className="login-signup">
+                <div className="ls-content">
+                  <button className="login-btn" onClick={this.toggleLogin.bind(this)}>Login</button>
+                  <button className="signup-button" onClick={this.toggleSignup.bind(this)}>Signup</button>
+                </div>
+              </div>
               <Row className="login-form">
                 <Col md={10} mdOffset={1}>
-                  <Login />
+                {this.state.isLogin ? <Login /> : <Signup/>}
                 </Col>
               </Row>
             </Col>
